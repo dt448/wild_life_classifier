057fadf868c874a8d92695acd10548086c00a220
DenThinkPad <dt448@users.noreply.github.com>
2023-01-23 08:10:33 +0000

    rollback to toy example

--- app.py
+++ app.py
@@ -10,40 +10,10 @@ import sklearn
 # import tensorflow as tf
 from keras.applications.resnet_v2 import preprocess_input
 
-# st.title('British Wild Life Classifier')
-# st.text('Upload an image to classify')
-# model = pickle.load(open("clf.p", 'rb'))
-
-# uploaded_file = st.file_uploader("Choose an image ...", type ="jpg")
-# if uploaded_file is not None:
-#     img = Image.open(uploaded_file)
-#     st.image(img, caption = 'Uploaded Image')
-#     img = np.array(img)
-    
-#     if st.button('PREDICT'):
-#         CATEGORIES = ['pigeon', 'daffodil', 'fox']
-#         st.write('Results...')
-#         flat_data = []
-#         img_resized = resize(img,(150,150,3))
-#         flat_data.append(img_resized.flatten())
-#         flat_data = np.array(flat_data)
-#         y_out = model.predict(flat_data)
-#         y_out = CATEGORIES[y_out[0]]
-#         st.title(f' This is a {y_out}')
-#         q = model.predict_proba(flat_data)
-#         st.write("But I am only:")
-#         for index, item in enumerate(CATEGORIES):
-#             st.write(f'{q[0][index]*100}% sure it is a {item}')
-
-# ResNet Model:
-
-model = pickle.load(open("resnet_model_v1.p", 'rb'))
-
-def load_image(image):
-    image = image.resize((224,224))
-    img_array = np.array(image)#/255 # a normalised 2D array                
-    img_array = img_array.reshape(-1, 224, 224, 3)   # to shape as (1, 224, 224, 3)
-    return img_array
+st.title('British Wild Life Classifier')
+st.text('Note: This is only a toy example (classifies fox, pigeon and daffodil - please see GitHub for ResNet model.)')
+st.text('Upload an image to classify')
+model = pickle.load(open("clf.p", 'rb'))
 
 uploaded_file = st.file_uploader("Choose an image ...", type ="jpg")
 if uploaded_file is not None:
@@ -52,31 +22,62 @@ if uploaded_file is not None:
     img = np.array(img)
     
     if st.button('PREDICT'):
-        # CATEGORIES = ['pigeon', 'daffodil', 'fox']
-        CATEGORIES = ["fox", "pigeon", "daffodil", "badger","bank vole", "barbastelle bat", "bechstein's bat", "field vole", "lynx", "otter", "pine marten"]
+        CATEGORIES = ['pigeon', 'daffodil', 'fox']
         st.write('Results...')
         flat_data = []
-        img = load_image(Image.open(uploaded_file))
-        # img_resized = resize(img,(224,224,3))
-        # flat_data.append(img_resized.flatten())
-        # flat_data = np.array(flat_data)
-
-        # my_image = img_to_array(my_image)
-        # my_image = my_image.reshape((1, my_image.shape[0], my_image.shape[1], my_image.shape[2]))
-        # my_image = preprocess_input(img)
-        my_image = img
-
-        #make the prediction
-        prediction = model.predict(my_image)
-        print("predictions:",prediction)
-
-        pred = np.argmax(prediction, axis=-1)
-
-        # y_out = model.predict(flat_data)
-        y_out = CATEGORIES[pred[0]]
+        img_resized = resize(img,(150,150,3))
+        flat_data.append(img_resized.flatten())
+        flat_data = np.array(flat_data)
+        y_out = model.predict(flat_data)
+        y_out = CATEGORIES[y_out[0]]
         st.title(f' This is a {y_out}')
-
-        # q = model.predict_proba(flat_data)
+        q = model.predict_proba(flat_data)
         st.write("But I am only:")
         for index, item in enumerate(CATEGORIES):
-            st.write(f'{prediction[0][index]*100}% sure it is a {item}') 
+            st.write(f'{q[0][index]*100}% sure it is a {item}')
+
+# ResNet Model:
+
+# model = pickle.load(open("resnet_model_v1.p", 'rb'))
+
+# def load_image(image):
+#     image = image.resize((224,224))
+#     img_array = np.array(image)#/255 # a normalised 2D array                
+#     img_array = img_array.reshape(-1, 224, 224, 3)   # to shape as (1, 224, 224, 3)
+#     return img_array
+
+# uploaded_file = st.file_uploader("Choose an image ...", type ="jpg")
+# if uploaded_file is not None:
+#     img = Image.open(uploaded_file)
+#     st.image(img, caption = 'Uploaded Image')
+#     img = np.array(img)
+    
+#     if st.button('PREDICT'):
+#         # CATEGORIES = ['pigeon', 'daffodil', 'fox']
+#         CATEGORIES = ["fox", "pigeon", "daffodil", "badger","bank vole", "barbastelle bat", "bechstein's bat", "field vole", "lynx", "otter", "pine marten"]
+#         st.write('Results...')
+#         flat_data = []
+#         img = load_image(Image.open(uploaded_file))
+#         # img_resized = resize(img,(224,224,3))
+#         # flat_data.append(img_resized.flatten())
+#         # flat_data = np.array(flat_data)
+
+#         # my_image = img_to_array(my_image)
+#         # my_image = my_image.reshape((1, my_image.shape[0], my_image.shape[1], my_image.shape[2]))
+#         # my_image = preprocess_input(img)
+#         my_image = img
+
+#         #make the prediction
+#         prediction = model.predict(my_image)
+#         print("predictions:",prediction)
+
+#         pred = np.argmax(prediction, axis=-1)
+
+#         # y_out = model.predict(flat_data)
+#         y_out = CATEGORIES[pred[0]]
+#         st.title(f' This is a {y_out}')
+
+#         # q = model.predict_proba(flat_data)
+#         st.write("But I am only:")
+#         for index, item in enumerate(CATEGORIES):
+#             st.write(f'{prediction[0][index]*100}% sure it is a {item}') 
--- resnet_model_v1.p
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:076297286d9b96ea14e8dd1494771d891188c8b569ec8914bab93267437505c3
-size 190720230
